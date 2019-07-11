680d3a715be410ede554778746e232f560ae7b02
HaiDV <hai.dinh@niteco.se>
2019-07-11 14:08:18 +0700

    Install aws cli on travis

--- .terraform/variables.tf
+++ .terraform/variables.tf
@@ -3,14 +3,6 @@ variable "aws_region" {
   default     = "ap-southeast-2"
 }
 
-variable "aws_access_key" {
-  description = "AWS Access key."
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret key."
-}
-
 variable "public_key_name" {
   description = "SSH Key name to connect your EC2."
 }
