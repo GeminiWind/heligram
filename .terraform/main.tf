c218d66758d46b2930e72c67accece1925cef06c
HaiDV <hai.dinh@niteco.se>
2019-07-12 17:35:29 +0700

    Fix: install docker script

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -82,15 +82,15 @@ resource "null_resource" "install_dependencies" {
   }
   provisioner "remote-exec" {
     inline = [
-      "sleep 20",
-      "sudo apt-get -y update",
-      "sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common",
+      "sudo apt-get update -y",
+      "sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo apt-key fingerprint 0EBFCD88",
-      "sudo apt-get -y update",
-      "sudo apt-get install docker-ce docker-ce-cli containerd.io",
+      "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable",
+      "sudo apt-get update -y",
+      "sudo apt-get install docker-ce -y",
       "sudo curl -L 'https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo mkdir -p /app"
     ]
   }
   depends_on = ["aws_instance.web"]
