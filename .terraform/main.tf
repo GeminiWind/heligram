dc9a33a3887f7aed6cb7e01cfc1e8ebfc5704f2f
HaiDV <hai.dinh@niteco.se>
2019-08-02 15:43:44 +0700

    Fix: user data

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -11,14 +11,6 @@ provider "cloudflare" {
   token = "${var.cloudflare_token}"
 }
 
-data "template_file" "install_dependencies" {
-  template = "${file("${path.module}/user-data.tpl")}"
-
-  vars = {
-    commit_sha = "${var.commit_sha}"
-  }
-}
-
 resource "tls_private_key" "privkey" {
     algorithm = "RSA"
     rsa_bits = 4096
@@ -37,7 +29,27 @@ resource "aws_instance" "web" {
 
   key_name = "${aws_key_pair.public_key.key_name}"
 
-  user_data = "${data.template_file.install_dependencies.rendered}"
+  user_data = <<EOF
+#! /bin/bash
+sleep 30
+sudo apt-get update -y
+sudo apt-get install git -ysudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+sudo service docker start
+sudo usermod -a -G docker ubuntu
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo chown root:docker /usr/local/bin/docker-compose
+docker-compose version
+rm -rf /tmp/heligram
+git clone https://github.com/GeminiWind/heligram /tmp/heligram
+cd /tmp/heligram
+git checkout ${var.commit_sha}
+docker-compose up -d
+EOF
 
   tags = {
     APP = "Heligram"
